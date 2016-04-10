af9ed8a71abf6e76410240a5ec9dc22babb2e2da
Tats.U <tats.u@live.jp>
2016-04-10 23:03:58 +0900

    Fixed the failure of an assert when the exponent is signed
    Left shift is always logical, not arithmetic in x64.

--- ipow.hpp
+++ ipow.hpp
@@ -19,7 +19,6 @@ T ipow(const T &base, const I &exp) {
   T ret = (T)1;
   static constexpr I maxval = std::numeric_limits<I>::max(); // (0)11111111111....
   static constexpr I highestbit = maxval ^ (maxval >> 1);    // (0)10000000000....
-  static_assert(maxval << 1 == I_0, "最上位ビットの生成ができませんでした。(Couldn't assign the MSB to `highestbit'.)");
   for(I mask = highestbit; mask != I_0; mask >>= 1) {
     ret *= ret;
     if((mask & exp) != I_0) ret *= base;
