5d8e1256900af8e5c17a1d0334e0897f7ecff2b6
Tatsunori Uchino <tats.u@live.jp>
2020-05-07 01:31:14 +0900

    Apply clang-format

--- ipow.hpp
+++ ipow.hpp
@@ -3,15 +3,17 @@
  * Copyright © 2017 UCHINO Tatsunori
  * This library is licensed under the zlib license.
  * https://opensource.org/licenses/Zlib
- * However, you can remove this notification only if you use this for closed-source and non-commercial purpose such as competitive programming and homework.
+ * However, you can remove this notification only if you use this for
+ * closed-source and non-commercial purpose such as competitive programming and
+ * homework.
  *
  * This computes x to nth power where n is a not-negative integer.
  */
-#include <type_traits>
 #include <limits>
 #include <stdexcept>
+#include <type_traits>
 
-template<typename T, typename I>
+template <typename T, typename I>
 #if __cplusplus >= 201402L
 constexpr T ipow(const T &base, const I &exp)
 #else
@@ -19,21 +21,23 @@ T ipow(const T &base, const I &exp)
 #endif
 {
   constexpr I I_0 = 0;
-  static_assert(std::is_integral<I>::value == true, "冪指数は整数型でなければなりません。(The exponent must be an integer.)");
+  static_assert(
+    std::is_integral<I>::value == true,
+    "冪指数は整数型でなければなりません。(The exponent must be an integer.)");
   T ret = (T)1;
-  constexpr I maxval = std::numeric_limits<I>::max(); // (0)11111111111....
-  constexpr I highestbit = maxval ^ (maxval >> 1);    // (0)10000000000....
-  if(std::is_signed<I>::value && exp < I_0) {
-      const I&& minus_exp_minus_1 = -(exp + 1);
-      for(I mask = highestbit; mask != I_0; mask >>= 1) {
-        ret *= ret;
-        if((mask & minus_exp_minus_1) != I_0) ret /= base;
+  constexpr I maxval = std::numeric_limits<I>::max();  // (0)11111111111....
+  constexpr I highestbit = maxval ^ (maxval >> 1);     // (0)10000000000....
+  if (std::is_signed<I>::value && exp < I_0) {
+    const I &&minus_exp_minus_1 = -(exp + 1);
+    for (I mask = highestbit; mask != I_0; mask >>= 1) {
+      ret *= ret;
+      if ((mask & minus_exp_minus_1) != I_0) ret /= base;
     }
     ret /= base;
   } else {
-    for(I mask = highestbit; mask != I_0; mask >>= 1) {
-        ret *= ret;
-        if((mask & exp) != I_0) ret *= base;
+    for (I mask = highestbit; mask != I_0; mask >>= 1) {
+      ret *= ret;
+      if ((mask & exp) != I_0) ret *= base;
     }
   }
   return ret;
